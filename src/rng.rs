f8f9f7e72563bd8a9c0b329356d9847ff66a9e12
Museus <museus@protonmail.com>
2024-05-01 09:33:04 -0400

    chore: remove dead SggPcg.advance function

--- src/rng.rs
+++ src/rng.rs
@@ -50,26 +50,6 @@ impl SggPcg {
     ///
     /// Using this function is equivalent to calling `next_32()` `delta`
     /// number of times.
-    #[inline]
-    pub fn advance(&mut self, delta: u64) {
-        let mut acc_mult: u64 = 1;
-        let mut acc_plus: u64 = 0;
-        let mut cur_mult = MULTIPLIER;
-        let mut cur_plus = INCREMENT;
-        let mut mdelta = delta;
-
-        while mdelta > 0 {
-            if (mdelta & 1) != 0 {
-                acc_mult = acc_mult.wrapping_mul(cur_mult);
-                acc_plus = acc_plus.wrapping_mul(cur_mult).wrapping_add(cur_plus);
-            }
-            cur_plus = cur_mult.wrapping_add(1).wrapping_mul(cur_plus);
-            cur_mult = cur_mult.wrapping_mul(cur_mult);
-            mdelta /= 2;
-        }
-        self.state = acc_mult.wrapping_mul(self.state).wrapping_add(acc_plus);
-    }
-
     pub fn new(seed: u64) -> Self {
         SggPcg {
             state: seed.wrapping_mul(MULTIPLIER).wrapping_sub(INITIAL_OFFSET),
